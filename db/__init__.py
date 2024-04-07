13c1ba06808cf85c8334a5a32b8f788f9b6bcf38
YodaFang <yodafang0121@gmail.com>
2024-04-07 11:48:03 +0300

    db opt

--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,4 @@
-from db.base import User
+from db.base import *
 
 # 设置__all__变量，指定可导出的内容
 __all__ = ['User', 'Product']
--- db/base.py
+++ db/base.py
@@ -1,9 +1,11 @@
 from peewee import *
-from playhouse.pool import PooledSqliteExtDatabase
+from playhouse.pool import PooledSqliteDatabase
 import os
+import datetime
 
 # db_file = os.getenv("DB_SQLLITE_FILE")
-db = PooledSqliteExtDatabase("db.data", max_connections=10)
+db_file = "db.data"
+db = PooledSqliteDatabase(db_file, max_connections=10)
 
 class Base(Model):
     class Meta:
@@ -16,9 +18,11 @@ class User(Base):
     join_date = DateTimeField()
 
 class Product(Base):
-    name_cn = CharField()
-    name_ro = CharField()
+    name_cn = CharField(500)
+    name_ro = CharField(500)
     bbd = DateField()
+    updated_at = DateTimeField(default=datetime.datetime.now)  # 自动更新的字段
 
-db.connect()
-db.create_tables([User, Product])
+if not os.path.exists(db_file):
+    db.connect()
+    db.create_tables([User, Product])
