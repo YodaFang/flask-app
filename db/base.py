15f560a865480d03d3d1692f6a78b6838fde0895
YodaFang <yodafang0121@gmail.com>
2024-04-07 21:09:56 +0300

    db opt

--- db/base.py
+++ db/base.py
@@ -17,7 +17,29 @@ class User(Base):
     email = CharField()
     join_date = DateTimeField()
 
+class Template(Base):
+    name = CharField(255)
+    desc = CharField(500, null=True)
+    width = CharField(100, null=True)
+    height = CharField(100, null=True)
+    font_size = CharField(100, null=True)
+    updated_at = DateTimeField(default=datetime.datetime.now)  # 自动更新的字段
+
+
+class Org(Base):
+    code = CharField(255, unique=True)
+    name1 = CharField(255, unique=True)
+    name2 = CharField(255, null=True)
+    name3 = CharField(255, null=True)
+    email = CharField(255, null=True)
+    tel = CharField(255, null=True)
+    addr = CharField(500, null=True)
+    website = CharField(500, null=True)
+    updated_at = DateTimeField(default=datetime.datetime.now)  # 自动更新的字段
+
 class Product(Base):
+    org = ForeignKeyField(Org, backref='products')
+    template = ForeignKeyField(Template)
     name_cn = CharField(500)
     name_ro = CharField(500)
     bbd = DateField()
@@ -25,4 +47,4 @@ class Product(Base):
 
 if not os.path.exists(db_file):
     db.connect()
-    db.create_tables([User, Product])
+    db.create_tables([User, Org, Product])
